59c3599d2b487ef57fe52d6d14e6775a02386641
U-LAPTOP-20B12E5U\digit <eric@digitalpour.com>
2020-09-24 18:10:50 +0200

    Fixed serial/parallel bug, fixed double soft clip bug

--- src/model/dsp/filter/NonlinearFunctions.hpp
+++ src/model/dsp/filter/NonlinearFunctions.hpp
@@ -5,13 +5,16 @@
  */
 template <typename T>
 T cubicSoftClip(T x, T drive) {
-    x = std::max(std::min(drive * x, (T) 1), (T) -1);
-    return (x - x*x*x / (T) 3) / drive;
+    // x = std::max(std::min(drive * x, (T) 1), (T) -1);
+    // return (x - x*x*x / (T) 3) / drive;
+    x = drive * x;
+    return std::max(std::min( (x - x*x*x / (T) 3) / drive, (T) 1), (T) -1);
 }
 
 template <typename T>
 T hardClip(T x, T drive) {
     x = std::max(std::min(drive * x, (T) 1), (T) -1);
+    //return x;
     return x / drive;
 }
 
@@ -23,10 +26,13 @@ T tanhClip(T x, T drive) {
 template <typename T>
 T doubleSoftClip(T x, T drive) {
     x = std::max(std::min(drive * x, (T) 1), (T) -1);
-    T u = x == 0.0 ? 0.0 : x > 0 ? x - 0.5 : x + 0.5;
+    //T in = x;
+    T u = x == 0.0 ? 0.0 : (x > 0 ? x - 0.5 : x + 0.5);
     x = 0.75 * (u - u*u*u / (T) 3);
-    x = u == 0.0 ? 0.0 : u > 0 ? x + 0.5 : x - 0.5;
-    return x / drive;
+    x = u == 0.0 ? 0.0 : (u > 0 ? x + 0.5 : x - 0.5);
+    //fprintf(stderr, "double soft clip in:%f drive:%f out%f  \n",in,drive,x);
+    //return x / drive;
+    return x;
 }
 
 
